6591d50fa28e550eb5400dd4438e6d6a95ce6b0b
Istiak Ahmed Tashrif <istiaktashrif@gmail.com>
2025-04-09 16:43:55 +0600

    done adding refresh token system

--- admin/src/api/axiosInstance.ts
+++ admin/src/api/axiosInstance.ts
@@ -1,8 +1,23 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`, 
-  withCredentials: true, 
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`,
+  withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    if (err.response?.status === 401) {
+      try {
+        await axiosInstance.post("/auth/refresh-token");
+        return axiosInstance(err.config);
+      } catch (refreshErr) {
+        return Promise.reject(refreshErr);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
 
 export default axiosInstance;
