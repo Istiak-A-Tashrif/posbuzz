c9721a6e46e58d519c3fb67c2a35f2fee763abb4
Istiak Ahmed Tashrif <istiaktashrif@gmail.com>
2025-04-19 04:57:42 +0600

    added a spinner until user available

--- admin/src/components/RequirePermission.tsx
+++ admin/src/components/RequirePermission.tsx
@@ -1,6 +1,7 @@
 import { JSX } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { Spin } from "antd";
 
 export default function RequirePermission({
   children,
@@ -11,6 +12,10 @@ export default function RequirePermission({
 }) {
   const { user } = useAuth();
 
+
+  if (!user) {
+    return <Spin/>
+  }
   // Check if the user has the required permission
   if (permission && !user?.permissions?.includes(permission)) {
     return <Navigate to="/403" replace />;
